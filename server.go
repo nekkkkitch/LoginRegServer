41796d7d25a1f20ffcaa71fde83526dd7c05bcf2
JonnykJr1337 <streamerpups@gmail.com>
2024-02-27 19:17:23 +0700

    InsertionInit

--- server.go
+++ server.go
@@ -16,12 +16,22 @@ type User struct {
 
 func MainPage(w http.ResponseWriter, r *http.Request) {
 	var users = GetUsers()
-	fmt.Println(users)
 	tmpl := template.Must(template.ParseFiles("templates/MainPage.html"))
 	tmpl.Execute(w, users)
 }
+func Login(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/LoginPage.html"))
+	user := User{
+		ID:       0,
+		Username: r.FormValue("Username"),
+		Password: r.FormValue("Password"),
+	}
+	tmpl.Execute(w, user)
+	fmt.Println(user)
+	//InsertUser(&user)
+}
 func GetUsers() []User {
-	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/mydb")
+	db, err := sql.Open("mysql", "root:JonnekJuar4002@tcp(127.0.0.1:3306)/mydb")
 	if err != nil {
 		panic(err.Error())
 	}
@@ -38,9 +48,21 @@ func GetUsers() []User {
 	}
 	return result
 }
+func InsertUser(u *User) {
+	db, err := sql.Open("mysql", "root:JonnekJuar4002@tcp(127.0.0.1:3306)/mydb")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+	_, err = db.Exec("insert into users(Username, Password) values (?, ?)", u.Username, u.Password)
+	if err != nil {
+		panic(err.Error())
+	}
+}
 func main() {
 	fs := http.FileServer(http.Dir("resources"))
 	http.Handle("/resources/", http.StripPrefix("/resources", fs))
 	http.HandleFunc("/", MainPage)
+	http.HandleFunc("/login/", Login)
 	http.ListenAndServe("localhost:8080", nil)
 }
